1f624385130051ace76c4bbae4c1219970f8eaf2
Rap Payne <bob@creator.net>
2020-02-28 16:07:55 -0600

    Split page layouts (#2)
    * Added vscode files
    
    * Cleaned solutions
    
    * Cleaned starters
    
    * Added solution for 03 Page Setup
    
    * Added details for the films
    
    * Changed name of a class
    
    * Changed name of several classes
    
    * Added 04 Effective CSS Styling solution
    
    * Added 05 Semantic Grouping solution
    
    * Added 06 CSS Positioning solution
    
    * Added 07 Layout Pages with CSS solution
    
    * Added 08 Layout Pages with flexbox solution
    
    * Added 09 Layout Pages with grid solution
    
    * Added 10 Responsive design solution
    
    * Added 11 modern CSS formatting solution
    
    * Minor formatting of flex
    
    * removed unneeded div
    
    * Added 12 Advanced selectors solution
    
    * Centered films list
    
    * Added 13 transforms solution
    
    * Added 14 tables solution
    
    * Formatting. Simple
    
    * Added 15 Forms solution

--- starters/12 HTML Tables/assets/js/toasts.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const toggle = () => t.classList.toggle('showToast');
-  const t = document.getElementsByClassName('toast')[0];
-  document.getElementById('go').addEventListener('click', toggle);
-  const allCloseButtons = document.getElementsByClassName('close');
-  for (const cb of allCloseButtons) {
-    cb.addEventListener('click', toggle);
-  }
-});
