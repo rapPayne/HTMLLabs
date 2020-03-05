6bea3a24cb034c0bd6d372dcc2fd32b642d8f656
rapPayne <bob@creator.net>
2020-03-05 17:31:58 -0500

    Building divs

--- demos/flexbox_vs_grid/flexbox_vs_grid.js
+++ demos/flexbox_vs_grid/flexbox_vs_grid.js
@@ -2,3 +2,12 @@
 export function sayHello() {
   console.log('worked')
 }
+
+export function makeBoxes(howMany) {
+  const boxes = [];
+  for (let i=0 ; i<howMany ; i++ ) {
+    const newDiv = document.createElement('div');
+    boxes.push(newDiv);
+  }
+  return boxes;
+}
